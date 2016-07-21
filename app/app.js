88f67468fb6c9b6591ba8e87cda1a717c39cff96
Jacky Fong <jacky.fong@rappfrance.com>
2016-07-21 12:18:11 +0200

    Remove jquery :jquerycry:

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 import NumberUtils from './utils/number-utils';
-import $ from 'jquery';
 import _ from 'lodash';
 import TweenMax from 'gsap';
 
 class App {
 
+
+
     constructor() {
+
+        this.launchAnimation = this.onScroll.bind(this);
 
         this.canScroll = true;
 
@@ -25,61 +28,13 @@ class App {
      */
     addListeners() {
 
+
         window.addEventListener('resize', this.onResize.bind(this));
         // TweenMax.ticker.addEventListener('tick', this.update.bind(this));
 
-        $(document).ready(() => {
-            $(window).bind('mousewheel', _.throttle((event) => {
-
-                if (this.canScroll == true) {
-                    if (event.originalEvent.wheelDelta <= -5) {
-
-                        console.log("down");
-                        $(window).unbind('mousewheel');
-
-                        var tl = new TimelineMax();
-
-                        // tl.add(TweenMax.to("#landing #center", 1, {scale: 0.4, top: 100, left: 30, ease:Power2.easeOut}));
-
-                        tl.to("#landing #bottom-scroll", 0.75, {y: 50, opacity:0, ease:Power2.easeOut}, "john")
-                            .to("#landing #center", .75, {y: 15, opacity:0, ease:Power2.easeOut, onComplete: () => {
-                                TweenMax.set("#landing", {display: "none"});
-                            }}, "john+=0.25");
-
-                    //     tl.add(TweenMax.to("#landing #bottom-scroll", 0.75, {y: 50, opacity:0, ease:Power2.easeOut}, "john"));
-                    //     tl.add(TweenMax.to("#landing #center", 0.75, {y: 50, opacity:0, ease:Power2.easeOut, onComplete: () => {
-                    //             TweenMax.set("#landing", {display: "none"});
-                    // }}, "john-=0.25"));
-
-                        // tl.add([TweenMax.to("#menu-border .menu-border_topbottom", 1, {height:60, ease:Power2.easeOut}),
-                        //     TweenMax.to("#menu-border .menu-border_leftright", 1, {width:60, ease:Power2.easeOut})]);
-
-                        tl.to("#menu-border .menu-border_topbottom", 1, {height:60, ease:Power2.easeOut}, "doe")
-                            .to("#menu-border .menu-border_leftright", 1, {width:60, ease:Power2.easeOut}, "doe")
-                            .fromTo("#menu-border #menu-border_left #logo-name", 0.75, {y: -100}, {y: 0, display:"block", opacity:1, ease:Power2.easeOut, force3D:false}, "doe -= 0.75")
-                            .set("#menu-border #menu-border_left #nav ul", {display: "block"}, "doe -= 0.75")
-                            .staggerFrom("#menu-border #menu-border_left #nav ul li", 0.75, {left: -100, opacity:0, ease:Power2.easeOut, force3D:true}, 0.2, "doe -= 0.5");
-
-                    //     tl.add(TweenMax.fromTo("#menu-border #menu-border_left #logo-name", 0.75, {top: -100}, {top: 50, display:"block", opacity:1, ease:Power2.easeOut, onComplete: () => {
-                    //             TweenMax.set("#menu-border #menu-border_left #nav ul", {display: "block"});
-                    // }}));
-                    //     tl.add(TweenMax.staggerFrom("#menu-border #menu-border_left #nav ul li", 0.75, {left: -100, opacity:0, ease:Power2.easeOut, force3D:true}, 0.2));
-
-                        // TweenMax.to("#landing #bottom-scroll", 0.5, {opacity:0, display:"none"});
-
-
-                    }
-                    else if (event.originalEvent.wheelDelta >= 35) {
-
-                        //console.log("up");
-                    }
-                }
-
-            }, 350));
+        document.addEventListener("DOMContentLoaded", () => {
+            window.addEventListener('wheel', this.launchAnimation);
         });
-
-
-
 
     }
 
@@ -104,6 +59,50 @@ class App {
 
         this.width = window.innerWidth;
         this.height = window.innerHeight;
+
+    }
+
+    onScroll(event) {
+        //console.log(event.wheelDeltaY);
+
+        if (this.canScroll == true) {
+            if (event.wheelDeltaY <= -5) {
+
+                console.log("down");
+                window.removeEventListener('wheel', this.launchAnimation);
+
+                var tl = new TimelineMax();
+
+                tl.to("#landing #bottom-scroll", 0.75, {y: 50, opacity: 0, ease: Power2.easeOut}, "john")
+                    .to("#landing #center", .75, {
+                        y: 15, opacity: 0, ease: Power2.easeOut, onComplete: () => {
+                            TweenMax.set("#landing", {display: "none"});
+                        }
+                    }, "john+=0.25");
+
+                tl.to("#menu-border .menu-border_topbottom", 1, {height: 60, ease: Power2.easeOut}, "doe")
+                    .to("#menu-border .menu-border_leftright", 1, {width: 60, ease: Power2.easeOut}, "doe")
+                    .fromTo("#menu-border #menu-border_left #logo-name", 0.75, {y: -100}, {
+                        y: 0,
+                        display: "block",
+                        opacity: 1,
+                        ease: Power2.easeOut,
+                        force3D: false
+                    }, "doe -= 0.75")
+                    .set("#menu-border #menu-border_left #nav ul", {display: "block"}, "doe -= 0.75")
+                    .staggerFrom("#menu-border #menu-border_left #nav ul li", 0.75, {
+                        left: -100,
+                        opacity: 0,
+                        ease: Power2.easeOut,
+                        force3D: true
+                    }, 0.2, "doe -= 0.5");
+
+            }
+            else if (event.wheelDeltaY >= 35) {
+
+                //console.log("up");
+            }
+        }
 
     }
 
